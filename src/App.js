e672e78578181c0231fe46928ff2592cec6520dc
Shubhom J <shubhom@geniusplaza.com>
2021-08-19 14:01:47 +0530

    feat: added image and change text

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={'/keymouseit.png'} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Deployment Test
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://keymouseit.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Shiven Juneja
         </a>
       </header>
     </div>
